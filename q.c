54c37bae54fc42cd9a5fc36c2bac8337d396a986
SyedaKissa <60823225+SyedaKissa@users.noreply.github.com>
2020-03-01 03:36:19 +0500

    Update q.c

---
+++ q.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+
+void atstart() {
+	clear(); 
+	printf(" \n \n ************* Kissa's Shell *************\n \n");
+    sleep(2);
+    clear();
+}
+
+int main()
+{
+    atstart();
+	return 0;
+}
